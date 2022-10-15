dfce3bde9af4e6dac296cd7de8df7933cd988c0a
raiabhi007 <abhishekrai192001@gmail.com>
2022-10-15 22:50:48 +0530

    disconnected part completed

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -38,8 +38,25 @@ const EditorPage = () => {
             setClients(clients);
           }
         )
-      }
+        //Listening for disconnected
+        socketRef.current.on(
+          ACTIONS.DISCONNECTED,
+          ({socketId,username})=>{
+            toast.success(`${username} left the room`);
+            setClients((prev)=>{
+              return prev.filter(
+                (client)=> client.socketId !==socketId
+              )
+            });
+          }
+        );
+      };
       init()
+      return ()=>{
+        socketRef.current.disconnect();
+        socketRef.current.off(ACTIONS.JOINED);
+        socketRef.current.off(ACTIONS.DISCONNECTED);
+      };
     },[]);
 
    
